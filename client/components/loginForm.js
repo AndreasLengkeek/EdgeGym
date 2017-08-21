eec273d2e64ad29c769d4d50f39ee9e32979c44a
Salvatore Jalaty <salvatore.j.jalaty@student.uts.edu.au>
2017-08-21 17:38:45 +1000

    Login Form now takes params and alerts the values

--- client/components/loginForm.js
+++ client/components/loginForm.js
@@ -1,21 +1,48 @@
 import React from 'react';
 
 export default class LoginForm extends React.Component {
+
+  constructor(props) {
+     super(props);
+     this.state = { email: "", password: "" };
+     this.handleSubmit = this.handleSubmit.bind(this);
+     this.handleEmailChange = this.handleEmailChange.bind(this);
+     this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handleEmailChange(e) {
+   this.setState({email: e.target.value.toString()});
+}
+handlePasswordChange(e) {
+   this.setState({password: e.target.value.toString()});
+}
+
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    alert("Email: " + this.state.email + " Password: " + this.state.password );
+    /*let loginInput = this.refs.login;
+    this.props.addCard(loginInput.value);
+    loginInput.value = '';*/
+  }
+
+
   render () {
     return (
         <div className="row">
         <div className="col-lg-3"></div>
     	<div className="col-lg-6">
-    	<form method="POST" action="login">
+    	<form id="LoginForm" onSubmit={this.handleSubmit}>
     		<h1>Welcome to Edge Gym </h1>
     		<h2>Login</h2>
             <div className="form-group">
     		<label for="email">Email</label>
-    		<input type="text" className="form-control" id="email"/>
+    		<input type="text" className="form-control" id="email" name="password" value={this.state.email} onChange={this.handleEmailChange}/>
             </div>
             <div className="form-group">
     		<label for="Password">Password</label>
-    		<input type="password" className="form-control" id="pwd" />
+    		<input type="password" className="form-control" id="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
     		</div>
             <input type="submit" value="Login" className="btn btn-primary"/>
 
