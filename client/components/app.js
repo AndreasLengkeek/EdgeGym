a53e47766bd12c3772360ef13b70741259b9f948
Salvatore Jalaty <salvatore.j.jalaty@student.uts.edu.au>
2017-08-21 11:38:38 +1000

    Updated index.js to show a landing page (Unable to run webpack so I am just pushing to confirm git repo is setup correctly"

--- client/components/app.js
+++ client/components/app.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default class App extends React.Component {
   render () {
-    return <h1>hello</h1>;
+    return (
+    	<div>
+    		<h1>Welcome to Edge Gym </h1>
+    		<h2>Login</h2>
+    	</div>
+    );
   }
 }
