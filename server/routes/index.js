4249397e5ee40bc2e0cd103beb114369f7c25131
AndreasLengkeek <andreas@propertybase.com>
2017-08-21 14:19:26 +1000

    Remove login from routes file

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,8 +5,4 @@ router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../../client/index.html'));
 });
 
-router.post('/login', function(req, res){
-res.sendFile(path.join(__dirname, '../../client/login.html'));
-});
-
 module.exports = router;
