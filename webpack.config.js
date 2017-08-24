07a0aa226e3e95da32d208e51c1ea402f15224d2
AndreasLengkeek <andreas@propertybase.com>
2017-08-25 01:51:28 +1000

    Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './client/index.js',
@@ -16,5 +17,12 @@ module.exports = {
         presets: ['es2015', 'react']
       }
     }]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: 'index.html',
+      inject: true
+    })
+  ]
 }
