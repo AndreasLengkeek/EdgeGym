aae9c7fbfdc491328d15612589aa4afe4349f74e
AndreasLengkeek <andreas@propertybase.com>
2017-10-11 22:59:35 +1100

    remove html webpack plugin

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
