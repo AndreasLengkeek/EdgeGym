56d97cd07facfec06876eb4c76aefd214017789e
Salvatore Jalaty <salvatore.j.jalaty@student.uts.edu.au>
2017-10-14 14:00:20 +1100

    Added sidebar brand to show image in nav

--- client/app/components/HeaderLoggedOut.js
+++ client/app/components/HeaderLoggedOut.js
@@ -24,9 +24,11 @@ export default class Header extends Component {
     return (
       <div id="sidebar-wrapper">
             <ul className="sidebar-nav">
-              <li className="sidebar-brand">
-                <Link to="/">Edge Gym</Link>
-              </li>
+            <li className="sidebar-brand">
+              <Link to="/">
+                <Image src="/banner.jpg" responsive/>
+              </Link>
+            </li>
               <li>
                 <NavLink to="/login">Login</NavLink>
               </li>
