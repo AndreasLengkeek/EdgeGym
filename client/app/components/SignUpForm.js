d0c8443ef3a53d172482643992f34d5ea2dab28b
Salvatore Jalaty <salvatore.j.jalaty@student.uts.edu.au>
2017-10-14 13:43:05 +1100

    Fixed signup for to use bootReact

--- client/app/components/SignUpForm.js
+++ client/app/components/SignUpForm.js
@@ -18,27 +18,28 @@ const SignUpForm = ({ onSubmit, onChange, errors, user}) => {
             value={user.firstname}
             onChange={onChange} />
 
-          <div className="form-group">
-              <label htmlFor="firstname">First Name</label>
-              <input type="text" className="form-control"
-              name="firstname" value={user.firstname} onChange={onChange}/>
-          </div>
-          <div className="form-group">
-              <label htmlFor="lastname">Last Name</label>
-              <input type="text" className="form-control"
-              name="lastname" value={user.lastname} onChange={onChange}/>
-          </div>
-          <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="text" className="form-control"
-              name="email" value={user.email} onChange={onChange}/>
-          </div>
-          <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="text" className="form-control"
-              name="password" value={user.password} onChange={onChange}/>
-          </div>
-          <input type="submit" value="Sign Up" className="btn btn-primary"/>
+          <FieldGroup
+            label="Last Name"
+            type="text"
+            name="firstname"
+            value={user.lastname}
+            onChange={onChange} />
+
+          <FieldGroup
+            label="Email"
+            name="email"
+            type="text"
+            value={user.email}
+            onChange={onChange} />
+
+          <FieldGroup
+            label="Password"
+            type="password"
+            name="password"
+            value={user.password}
+            onChange={onChange} />
+
+          <Button type="submit">Submit</Button>
 
           <h6>Already have an account? <Link to={'/login'}>Login</Link></h6>
         </Form>
