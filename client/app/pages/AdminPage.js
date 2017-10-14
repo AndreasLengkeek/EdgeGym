549cb8032bca0a3123513aaf9dcfbe874b7801a3
Salvatore Jalaty <salvatore.j.jalaty@student.uts.edu.au>
2017-10-14 14:45:02 +1100

    Added new page 'AdminPage'

---
+++ client/app/pages/AdminPage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import CoachCreateContainer from '../containers/CoachCreateContainer';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+export default class AdminPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Row>
+        <Col sm={6} smOffset={1}>
+        <h1>Create a new Coach</h1>
+        <CoachCreateContainer {...this.props} />
+      </Col>
+    </Row>
+    )
+  }
+}
