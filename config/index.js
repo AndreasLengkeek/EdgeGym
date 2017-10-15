e7b86ca85fa98980424f753a3940a5b16fa16eda
AndreasLengkeek <andreas@propertybase.com>
2017-10-15 18:14:45 +1100

    Add forgot password api endpoint

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   db: process.env.MONGODB_URI,
   jwtSecret: process.env.JWT_SECRET,
+  sendgridKey: process.env.SENDGRID_API_KEY,
   facebookAuth: {
       clientID: process.env.FBCLIENT_ID,
       clientSecret: process.env.FBCLIENT_SECRET,
